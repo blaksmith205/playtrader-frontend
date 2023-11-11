86c74e32459e0918b71a11af4a303e388cf37c35
William Blanc <blaksmith57@gmail.com>
2023-11-10 22:26:30 -0500

    Updates deploy script to properly elevate perms

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 echo "Building the server files..."
 npm run build
 echo "Replacing files..."
-rm -r /var/www/playtrader.org/html
-mkdir -p /var/www/playtrader.org/html && cp -r ./build/* /var/www/playtrader.org/html/
+sudo rm -r /var/www/playtrader.org/html
+sudo mkdir -p /var/www/playtrader.org/html && sudo cp -r ./build/* /var/www/playtrader.org/html/
 echo "Server deployed!"
-
